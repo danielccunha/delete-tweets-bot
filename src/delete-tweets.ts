c02b07f30cb4684f8367c5119429e16fab6ebf5d
Daniel Cunha <danielcunha54@gmail.com>
2021-08-16 21:49:25 -0300

    refactor: adapt script to also remove likes

--- src/delete-tweets.ts
+++ src/delete-tweets.ts
@@ -1,60 +1,25 @@
-import 'dotenv/config'
 import chalk from 'chalk'
 import dayjs from 'dayjs'
 
-import { BATCH_SIZE, MAXIMUM_DATE, USERNAME } from './config'
-import { twitter, Tweet } from './twitter'
-
-async function loadTweets(): Promise<Tweet[]> {
-  const tweets: Tweet[] = []
-  let maximumId: number
-
-  while (true) {
-    console.log(`Loading tweets with maximum ID ${chalk.yellow(maximumId)}`)
-    const response = await twitter.get('statuses/user_timeline', {
-      max_id: maximumId,
-      screen_name: USERNAME,
-      count: 20,
-      include_rts: 1
-    })
-
-    // Ignore first value if tweets is not empty because it returns the tweet with maximum ID
-    if (tweets.length) {
-      tweets.shift()
-    }
-
-    tweets.push(...(response as any))
-    const lastId = response.length ? response[response.length - 1].id : undefined
-    if (!lastId || lastId === maximumId || tweets.length >= BATCH_SIZE) {
-      break
-    }
-
-    maximumId = lastId
-  }
-
-  return tweets
-}
-
-async function deleteTweet(tweet: Tweet) {
-  await twitter.post(`statuses/destroy`, { id: tweet.id_str })
-}
+import 'dotenv/config'
+import { MAXIMUM_DATE, USERNAME } from './config'
+import { loadTweets, removeTweet } from './twitter'
 
 async function execute() {
   try {
-    // Load tweets from configured username
-    console.log(`Loading tweets from username ${chalk.blue(USERNAME)}`)
-    let tweets = await loadTweets()
+    const resource = 'tweet'
+    console.log(`Loading tweets to delete from username ${USERNAME}`)
+    let tweets = await loadTweets(resource)
 
-    // Filter tweets by maximum date
     const formattedDate = dayjs(MAXIMUM_DATE).format('MM/DD/YYYY')
     console.log(`Filtering tweets by maximum date ${chalk.blue(formattedDate)}`)
     tweets = tweets.filter(tweet => dayjs(tweet.created_at).isBefore(MAXIMUM_DATE))
     console.log(`Found ${chalk.blue(tweets.length)} tweets to delete`)
 
-    // Delete each of the found tweets
-    console.log(`Deleting tweets`)
-    const promises = tweets.map(deleteTweet)
-    await Promise.all(promises)
+    for (const tweet of tweets) {
+      console.log(`Removing tweet ${chalk.red(tweet.id)}`)
+      await removeTweet(resource, tweet)
+    }
   } catch (error) {
     console.error(error)
   }
--- src/twitter.ts
+++ src/twitter.ts
@@ -1,4 +1,7 @@
+import chalk from 'chalk'
 import Twitter from 'twitter'
+
+import { USERNAME, BATCH_SIZE } from './config'
 
 export const twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,5 +14,47 @@ export interface Tweet {
   id: number
   id_str: string
   text: string
+  user: {
+    id: number
+    screen_name: string
+  }
   created_at: string
+}
+
+export type Resource = 'tweet' | 'like'
+
+export async function loadTweets(resource: Resource): Promise<Tweet[]> {
+  const endpoint = resource === 'tweet' ? 'statuses/user_timeline' : 'favorites/list'
+  const tweets: Tweet[] = []
+  let maximumId: number
+
+  while (true) {
+    console.log(`Loading tweets with maximum ID ${chalk.yellow(maximumId)}`)
+    const response = await twitter.get(endpoint, {
+      max_id: maximumId,
+      screen_name: USERNAME,
+      count: 200,
+      include_rts: 1
+    })
+
+    // Ignore first value if tweets is not empty because it returns the tweet with maximum ID
+    if (tweets.length) {
+      tweets.shift()
+    }
+
+    tweets.push(...(response as any))
+    const lastId = response.length ? response[response.length - 1].id : undefined
+    if (!lastId || lastId === maximumId || tweets.length >= BATCH_SIZE) {
+      break
+    }
+
+    maximumId = lastId
+  }
+
+  return tweets
+}
+
+export async function removeTweet(resource: Resource, tweet: Tweet) {
+  const endpoint = resource === 'tweet' ? 'statuses/destroy' : 'favorites/destroy'
+  await twitter.post(endpoint, { id: tweet.id_str })
 }
